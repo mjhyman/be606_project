243046b75ba21c0bb09ef1522a677207b36656fa
Mackenzie Hyman <mackenzie.hyman@gmail.com>
2022-03-30 17:20:25 -0400

    Combined Andrews script with ODE script. Now overlaying raw data with ODE plot

--- Raw_Data_Plot.m
+++ Raw_Data_Plot.m
@@ -1,12 +1,19 @@
 %import txt files
-a = importdata('Exercise A.txt');
-b = importdata('Exercise B.txt');
-c = importdata('Exercise C.txt');
-d = importdata('Exercise D.txt');
-e = importdata('Exercise E.txt');
-f = importdata('Exercise F.txt');
-g = importdata('Exercise G.txt');
-h = importdata('Exercise H.txt');
+clear; clc; close all;
+% 'Exercise A,B' = exercise 2 on/off. Velocity = 13.4 Km/hr
+% 'Exercise C,D' = exercise 3 on/off. velocity = 14.4 Km/hr
+% 'Exercise E,F' = exercise 4 on/off. velocity = 15.7 Km/hr
+wkdir = 'S1_Dataset';
+
+% Exercise 2
+a = importdata(fullfile(wkdir, 'Exercise A.txt'));
+b = importdata(fullfile(wkdir, 'Exercise B.txt'));
+% Exercise 3
+c = importdata(fullfile(wkdir, 'Exercise C.txt'));
+d = importdata(fullfile(wkdir, 'Exercise D.txt'));
+% Exercise 4
+e = importdata(fullfile(wkdir, 'Exercise E.txt'));
+f = importdata(fullfile(wkdir, 'Exercise F.txt'));
 
 %resize matrix to remove zeros
 at = a(1:962,1);
@@ -27,17 +34,10 @@ ehr = e(1:933,2);
 ft = f(1:786,1);
 fhr = f(1:786,2);
 
-gt = g(1:860,1);
-ghr = g(1:860,2);
-
-ht = h(1:748,1);
-hhr = h(1:748,2);
-
 %add time from exercise time vector to recovery vector
 btnew = bt + at(end,1);
 dtnew = dt+ct(end,1);
 ftnew = ft + et(end,1);
-htnew = ht + gt(end,1);
 
 %append time and heart rate vectors
 ex2t = [at; btnew];
@@ -46,8 +46,6 @@ ex3t = [ct; dtnew];
 ex3hr = [chr; dhr];
 ex4t = [et; ftnew];
 ex4hr = [ehr; fhr];
-ex5t = [gt; htnew];
-ex5hr = [ghr; hhr];
 
 %plot figures
 figure
@@ -64,11 +62,4 @@ figure
 plot(ex4t,ex4hr);
 xlabel('Time (s)');
 ylabel('hr (beats/min)');
-
-figure
-plot(ex5t,ex5hr);
-xlabel('Time (s)');
-ylabel('hr (beats/min)');
-
-
 
